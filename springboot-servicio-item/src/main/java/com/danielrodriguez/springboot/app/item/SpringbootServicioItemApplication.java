be49ce82aa12eb6ae635f4cd63bcd81f1aec8efd
Daniel Alejandro Rodriguez Gomez <darodriguez@personalsoft.com.co>
2022-08-03 15:10:35 -0500

    Avance Seccion 3 - Video 25

--- springboot-servicio-item/src/main/java/com/danielrodriguez/springboot/app/item/SpringbootServicioItemApplication.java
+++ springboot-servicio-item/src/main/java/com/danielrodriguez/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,11 +2,11 @@ package com.danielrodriguez.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@RibbonClient("servicio-productos")
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
 
